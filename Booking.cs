36dc8f93061abf88ad7e8e297a6226e8b00ea1a6
dodjislavich <nolik353@gmail.com>
2023-05-04 03:26:14 +1000

    добавил классы: User, Booking

---
+++ Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolodosServer
+{
+    public class Booking
+    {
+        public uint BookingId;
+        public int Date;
+        public int Time;
+        public uint UserId;
+
+        public Booking() { }
+
+        public Booking(uint _BookingId, int _Date, int _Time, uint _UserId)
+        {
+            BookingId = _BookingId;
+            Date = _Date;
+            Time = _Time;
+            UserId = _UserId;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HolodosServer
+{
+    public class User
+    {
+        public uint Id { get; }
+        public string Name;
+        public string Password;
+        public bool IsAdmin;
+        public bool IsVip;
+        public User() { }
+        public User(uint _Id, string _Name, string _Password, bool _IsAdmin, bool _IsVip)
+        {
+            Id = _Id;
+            Name = _Name;
+            Password = _Password;
+            IsAdmin = _IsAdmin;
+            IsVip = _IsVip;
+        }
+    }
+}
