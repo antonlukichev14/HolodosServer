5979c74003c4ab9e9a67e23e63f251426f267993
antonlukichev14 <antonlukichev14@gmail.com>
2023-05-09 22:31:53 +1000

    Revert "добавил классы: User, Booking"
    This reverts commit 2bd679c85c73408053bf13865b215b192c8162c1.

--- Database.cs
+++ Database.cs
@@ -1,50 +1,12 @@
-﻿using Microsoft.Office.Interop.Excel;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HolodosServer
 {
     class Database
     {
-        string filePath;
-
-        Application excel;
-        Workbook wb;
-        public Worksheet ws;
-
-        public Database()
-        {
-
-            filePath = @"DATABASEPLS.xlsx";
-            excel = new Application();
-            wb = excel.Workbooks.Open(filePath);
-            ws = wb.Worksheets[1];
-
-
-        }
-        public string ReadCell(int column, int row)
-        {
-            string cellValue = ws.Cells[column, row].Value2.ToString();
-
-            wb.Close();
-            excel.Quit();
-
-            return cellValue;
-        }
-
-        public void WriteCell(int column, int row, string value)
-        {
-
-            excel.Visible = true;
-            excel.DisplayAlerts = false;
-
-            ws.Cells[column, row].Value2 = value;
-
-            wb.SaveAs(@"DATABASEPLS.xlsx");
-            wb.Close();
-            excel.ActiveWorkbook.Close();
-            excel.Quit();
-            GC.Collect();
-        }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
-﻿namespace HolodosServer
+﻿using System;
+
+namespace HolodosServer
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Database database = new Database();
-            LogIn logIn = new LogIn();
-            User user = new User(14, "Ivan", "12345", "ivangay", false, false);
-            logIn.CreateNewUser(user);
-
-
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
