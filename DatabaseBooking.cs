a2e41911fb39e8d8973d5eac4792568901778ff0
antonlukichev14 <antonlukichev14@gmail.com>
2023-05-10 13:13:35 +1000

    Изменение базовых классов

---
+++ DatabaseBooking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolodosServer
+{
+    namespace Database
+    {
+        //Низкоуровневый класс для работы с базой данных записи
+        //Тут всё по работе с файлами
+        //Скрыт под namespace, поскольку большинству не нужен
+        static class DatabaseBooking
+        {
+
+        }
+    }
+}
---
+++ DatabaseUsersManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolodosServer
+{
+    //Высокоуровневый класс для работы с базой данных пользователей
+    //Здесь все функции, с которыми придётся работать другим разрабочикам
+    //P.S. бывший Login.cs
+    static class DatabaseUsersManager
+    {
+        public static bool LogInProfile(string login, string password)
+        {
+            return true;
+        }
+
+        public static bool CreateNewUser(User user)
+        {
+            return true;
+        }
+    }
+}
