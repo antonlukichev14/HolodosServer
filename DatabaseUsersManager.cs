98ec71e2ceec5fefbcb9b6c5798285c043190dbd
antonlukichev14 <antonlukichev14@gmail.com>
2023-05-28 19:46:39 +1000

    Небольшие изменения

--- DatabaseUsersManager.cs
+++ DatabaseUsersManager.cs
@@ -11,19 +11,19 @@ namespace HolodosServer
     //P.S. бывший Login.cs
     static class DatabaseUsersManager
     {
-        public static bool LogInProfile(string login, string password)
+        public static User UserEnter(string login, string password)
+        {
+            return new User();
+        }
+
+        public static bool CreateNewUser(string UserName, string UserLogin, string UserPassword)
         {
             return true;
         }
 
-        public static bool CreateNewUser(User user)
+        public static bool UserLoginCheck(string login) // тут функция которая сопоставляет логин с базой данных
         {
             return true;
-        }
-        public static User UserLoginCheck(string login) // тут должна быть функция которая сопоставляет логин и/или пароль с экземпляром User
-        {
-            User user = null;
-            return user;
         }
 
         public static User GetUser(string login) //функция ищет пользователя с указанным логином и возвращает экземпляр User с данными из базы данных
--- Server.cs
+++ Server.cs
@@ -11,16 +11,18 @@ namespace HolodosServer
         List<(User, DateTime)> currentUsers = new List<(User, DateTime)>();
         bool LogIn(string UserLogin, string UserPassword)
         {
-            if (DatabaseUsersManager.LogInProfile(UserLogin, UserPassword))
+            User currentUser = DatabaseUsersManager.UserEnter(UserLogin, UserPassword);
+
+            if (currentUser != null)
             {
-                currentUsers.Add((DatabaseUsersManager.UserLoginCheck(UserLogin), DateTime.UtcNow));
-                Console.WriteLine("Вы успешно авторизовались");
+                currentUsers.Add((currentUser, DateTime.UtcNow));
+                Console.WriteLine("Пользователь {0} авторизовался", currentUser.Login);
                 return true;
             }
             else
             {
                 // вывод оповещения о неверно введенных данных
-                Console.WriteLine("Произошла ошибка, проверьте корректность введеных данных");
+                Console.WriteLine("Попытка входа с неверно введёнными данными");
                 return false;
             }
 
@@ -28,20 +30,20 @@ namespace HolodosServer
 
         bool Registration(string UserName,string UserLogin, string UserPassword)
         {
-            if (DatabaseUsersManager.UserLoginCheck(UserLogin) == null)
+            if (DatabaseUsersManager.UserLoginCheck(UserLogin))
             {
-                User user = new User(UserName, UserLogin, UserPassword);
-                Console.WriteLine("Вы успешно зарегистрировались");
-                LogIn(UserLogin, UserPassword);
+                Console.WriteLine("Регистрация нового пользователя {0}", UserName);
+                DatabaseUsersManager.CreateNewUser(UserName, UserLogin, UserPassword);
                 return true;
             }
             else
             {
-                Console.WriteLine("Пользователь с таким login уже существует, попробуйте другой");
+                Console.WriteLine("Попытка регистрации с занятым логином");
                 return false;
             }
             
         }
+
         void FreezerBooking(string UserLogin, Booking UserBooking)
         {
 
