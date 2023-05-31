169bd5f48156f43b407b0ca8adde38f2273ca981
dodjislavich <nolik353@gmail.com>
2023-06-01 00:04:31 +1000

    Сделал процесс резервации, ожидю проверки

--- Server.cs
+++ Server.cs
@@ -45,8 +45,72 @@ namespace HolodosServer
 
         }
 
-        public static void FreezerBooking(string UserLogin, Booking UserBooking)
+        byte FreezerBooking(string UserLogin, Booking UserBooking)
         {
+            Booking[] DayBooking = new Booking[]; DayBooking = DatabaseBookingManager.DayRec(UserBooking.CityId, UserBooking.PlaceId, UserBooking.Date);
+            Booking[] WeekBooking = new Booking[]; WeekBooking = DatabaseBookingManager.WeekRec();
+            User Actuall = new User(); Actuall = DatabaseUsersManager.GetUser(UserLogin);
+
+            if (currentUsers.Exists(x => x.Item1 == Actuall) == false)
+            {
+                Console.WriteLine("Код ошибки 0; Пользователь не в currentUsers")
+                return (0)
+            }
+            //проверка на нахождениу пользователя в currentUsers
+
+            for(int i = 0; i <= WeekBooking.Length; i++)
+            {
+                if (Actuall.IsVip == 0 && Actuall.IsAdmin == 0)
+                {
+                    if (WeekBooking[i].UserId == Actuall.Id)
+                    {
+                        Console.WriteLine("Код ошибки 3;Пользователь превышает максимальный лимит записей (1)");
+                        return (3)
+                    }
+                }
+                if (Actuall.IsVip == 1 && Actuall.IsAdmin == 0 )
+                {
+                    int k == 0;
+                    if (WeekBooking[i].UserId == Actuall.Id) { k += 1; }
+                    if (k >= 3)
+                    {
+                        Console.WriteLine("Код ошибки 4;Пользователь превышает максимальный лимит записей (3)");
+                        return (4)
+                    }
+                } 
+            }
+
+            // проверка на кол-во записей за неделю завершена
+
+            List<int> OccupiedTime = new List<int>
+
+            for(int i = 0; i <= DayBooking.Length; i++)
+            {
+                for(int j = 0; j <= DayBooking[i].Hours; j++)
+                {
+                    int Hour = DayBooking[i].Time + j;
+                    OccupiedTime.Add(Hour);
+                }
+            }
+
+            for(int i = 0; i <= UserBooking.Hours; i++)
+            {
+                int UserHour = UserBooking.Time + i;
+                for(int j = 0; j <= OccupiedTime.Count; j++)
+                {
+                    if(UserHour == OccupiedTime[j])
+                    {
+                        Console.WriteLine("Код ошибки 2; Время уже занято")
+                        return (2);
+                    }
+                }
+            }
+
+            // проверка на незанятое время завершено
+
+            Console.WriteLine("Код исхода 1; Пользователь успешно забронировал холодильник");
+            return (1);
+
 
         }
     }
